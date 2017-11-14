587f705d800ec726afb7926754c4c356e89234c9
Younghoon Kim <the_basic_@kookmin.ac.kr>
2017-11-14 13:44:18 +0900

    Edit code

--- lineTracing.py
+++ lineTracing.py
@@ -16,7 +16,9 @@ def lineTracing():
         if current_dis <= standard_dis:
             go_forward_diff(90, 0)
             sleep(0.5)
-            go_forward_diff(30, 50)
+            go_forward_diff(45, 45)
+            sleep(1)
+            go_forward_diff(30, 60)
             sleep(1)
 
             # =====================================
@@ -58,13 +60,13 @@ def lineTracing():
             go_forward_diff(0, 0)
         # 중앙 감지
         elif OTD == 1 and OTB == 1 and OTA == 0 and OTC == 1 and OTE == 1:
-            go_forward_diff(45, 45)
+            go_forward_diff(70, 70)
         # 왼쪽으로 치우친 중앙 감지
         elif OTB == 0:
-            go_forward_diff(20, 55)
+            go_forward_diff(20, 65)
         # 오른쪽으로 치우친 중앙 감지
         elif OTC == 0:
-            go_forward_diff(55, 20)
+            go_forward_diff(65, 20)
         # 왼쪽으로 심하게 치우침
         elif OTD == 0:
             go_forward_diff(5, 90)
